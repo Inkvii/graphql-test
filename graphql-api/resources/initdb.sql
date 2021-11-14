981757237a5bf559931fd03013f264140385154c
Lubos Verzich <lve@paymorrow.com>
2021-11-14 23:11:17 +0100

    Added database schema

--- graphql-api/resources/initdb.sql
+++ graphql-api/resources/initdb.sql
@@ -1,5 +1,78 @@
 BEGIN;
 
+create user sa
+    superuser
+    createdb
+    createrole
+    replication
+    bypassrls;
+
+-- auto-generated definition
+create database backend_db
+    with owner sa;
+
+-- auto-generated definition
+create sequence if not exists item_item_id_seq
+    as integer;
+
+alter sequence item_item_id_seq owner to sa;
+
+alter sequence item_item_id_seq owned by item.item_id;
+
+-- auto-generated definition
+create sequence if not exists item_user_id_seq
+    as integer;
+
+alter sequence item_user_id_seq owner to sa;
+
+alter sequence item_user_id_seq owned by item.user_id;
+
+-- auto-generated definition
+create sequence if not exists user_id_seq
+    as integer;
+
+alter sequence user_id_seq owner to sa;
+
+alter sequence user_id_seq owned by "user".user_id;
+
+
+-- auto-generated definition
+create table if not exists item
+(
+    item_id serial
+        constraint item_pk
+            primary key,
+    name    text           not null,
+    price   numeric(19, 2) not null,
+    user_id serial
+        constraint item_user_user_id_fk
+            references "user"
+);
+
+comment on table item is 'item of the user';
+
+alter table item
+    owner to sa;
+
+
+-- auto-generated definition
+create table if not exists "user"
+(
+    user_id    integer default nextval('user_id_seq'::regclass) not null
+        constraint user_pk
+            primary key,
+    first_name text                                             not null,
+    last_name  text                                             not null,
+    age        integer default 0
+);
+
+alter table "user"
+    owner to sa;
+
+
+-- end of generating schema
+
+
 INSERT INTO "user" (first_name, last_name, age)
 VALUES ('Alois', 'Jirásek', 120),
        ('Beatrix', 'Schrödinger', 69),
