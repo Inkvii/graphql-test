b6f8b8108b05e721f7889c781ed4488539922cb2
Lubos Verzich <lve@paymorrow.com>
2021-11-17 10:02:30 +0100

    Added dynamic labels with inputs

--- frontend/components/Filter.tsx
+++ frontend/components/Filter.tsx
@@ -1,29 +1,36 @@
-import {useState} from "react"
+import {Fragment, useEffect, useState} from "react"
 import {UserCondition} from "graphql/types"
+
+
+interface UserFormDto {
+	fromId: string,
+	toId: string,
+	firstName: string,
+	lastName: string,
+	userId: string
+}
+
+const initialState: UserFormDto = {fromId: "", userId: "", firstName: "", lastName: "", toId: ""}
 
 interface Props {
 	refetch: CallableFunction
 }
 
-export default function Filter(props: Props) {
-	const [fromId, setFromId] = useState<string>("")
-	const [toId, setToId] = useState<string>("")
-	const [firstName, setFirstname] = useState<string>("")
-	const [lastName, setLastname] = useState<string>("")
-	const [userId, setUserId] = useState<string>("")
 
+export default function Filter(props: Props) {
+	const [dto, setDto] = useState<UserFormDto>(initialState)
 
 	const onFilter = () => {
 
 		const condition: UserCondition = {
-			firstName: firstName.length > 0 ? firstName : undefined,
-			lastName: lastName.length > 0 ? lastName : undefined,
-			userId: parseInt(userId) || undefined
+			firstName: dto.firstName.length > 0 ? dto.firstName : undefined,
+			lastName: dto.lastName.length > 0 ? dto.lastName : undefined,
+			userId: parseInt(dto.userId) || undefined
 		}
 
 		const queryParams = {
-			fromId: parseInt(fromId) || null,
-			toId: parseInt(toId) || null,
+			fromId: parseInt(dto.fromId) || null,
+			toId: parseInt(dto.toId) || null,
 			condition
 		}
 		console.log(JSON.stringify(queryParams))
@@ -33,12 +40,17 @@ export default function Filter(props: Props) {
 
 	const onReset = () => {
 		console.log("Resetting")
-		setFromId("")
-		setToId("")
-		setFirstname("")
-		setLastname("")
-		setUserId("")
+		setDto({...initialState})
 	}
+
+	const setValue = (property: keyof UserFormDto, value: string) => {
+		setDto({...dto, [property]: value})
+	}
+
+	useEffect(() => {
+		console.log(dto)
+	}, [dto])
+
 
 	return (
 		<div className={"my-2"}>
@@ -46,16 +58,15 @@ export default function Filter(props: Props) {
 			<div className={"flex"}>
 
 				<div className={"flex flex-col w-full"}>
-					<label>From id</label>
-					<input type={"text"} className={"border p-1 my-1"} value={fromId} onChange={e => setFromId(e.target.value)}/>
-					<label>To id</label>
-					<input type={"text"} className={"border p-1 my-1"} value={toId} onChange={e => setToId(e.target.value)}/>
-					<label>First name</label>
-					<input type={"text"} className={"border p-1 my-1"} value={firstName} onChange={e => setFirstname(e.target.value)}/>
-					<label>Last name</label>
-					<input type={"text"} className={"border p-1 my-1"} value={lastName} onChange={e => setLastname(e.target.value)}/>
-					<label>User id</label>
-					<input type={"text"} className={"border p-1 my-1"} value={userId} onChange={e => setUserId(e.target.value)}/>
+					{Object.keys(dto).map((key) => {
+						return (
+							<Fragment key={key}>
+								<label>{key}</label>
+								<input type={"text"} className={"border p-1 my-1"} value={dto[key as unknown as keyof UserFormDto]}
+								       onChange={e => setValue(key as unknown as keyof UserFormDto, e.target.value)}/>
+							</Fragment>
+						)
+					})}
 				</div>
 			</div>
 			<div className={"grid grid-cols-2 gap-1"}>
@@ -65,3 +76,5 @@ export default function Filter(props: Props) {
 		</div>
 	)
 }
+
+
